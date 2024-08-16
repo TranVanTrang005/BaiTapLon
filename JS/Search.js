43133052f6bb13b66f7a4bae36417eac97c273c3
TranVanTrang005 <trangit230305@gmail.com>
2024-08-16 10:28:22 +0700

    update

---
+++ JS/Search.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+    let search = this.document.getElementById("search").value;
+    let btn = this.document.querySelector(".searchButton");
+    let info = this.document.querySelectorAll(".info-box h3");
+    btn.onclick = function() {
+        
+    }
+})
